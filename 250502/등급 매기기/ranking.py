d80735ba3c7203cc7717bdb5da152ec65c1c9c53
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-02 15:59:04 +0900

    Title: 등급 매기기, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250502/등급 매기기/ranking.py
+++ 250502/등급 매기기/ranking.py
@@ -1,6 +1,7 @@
 grade = int(input())
 
 if grade >= 90: print('A')
-if grade >= 90: print('A')
-if grade >= 90: print('A')
-if grade >= 90: print('A')
+elif 80<=grade <90: print('B')
+elif 70<=grade <80: print('C')
+elif 60<=grade <70: print('D')
+else grade < 60: print('F')
