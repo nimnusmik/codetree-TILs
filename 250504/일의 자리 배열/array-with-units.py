625acde67912577a8484e24fdcf6877775e2435d
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-04 15:07:42 +0900

    Title: 일의 자리 배열, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250504/일의 자리 배열/array-with-units.py
+++ 250504/일의 자리 배열/array-with-units.py
@@ -1,8 +1,9 @@
 f, s = map(int, input().split())
-new_list = [f, s, f + s]   
+new_list = [f, s, f + s]  # ← 여기 'f + s'는 그냥 더한 값 그대로 넣기!
 
-for i in range(3, 10):
-    new_num = (new_list[i - 1] + new_list[i - 2]) % 10
+for i in range(3, 10):  # 10개 채울 때까지 반복
+    new_num = (new_list[i - 1] + new_list[i - 2]) % 10  # 그다음부터는 %10
     new_list.append(new_num)
 
-print(*new_list)
+for i in range(10):  # 10개까지만 출력
+    print(new_list[i], end=' ')
