596a24881ff65283ba89458b95cff6afd3c27372
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-04 17:46:25 +0900

    Title: 1부터의 합, Time: 48ms, Memory: 20MB, Status: Passed - Codetree

--- 250504/1부터의 합/sum-from-the-one.py
+++ 250504/1부터의 합/sum-from-the-one.py
@@ -2,9 +2,10 @@ num = int(input())
 sum = 0
 count = 0
 
-for i in range(1,num+1):
-    if sum < i: 
-        sum += i
-        count += 1
+for i in range(1, num + 1):
+    sum += i
+    count += 1
+    if sum > num:
+        break
+
 print(count)
-    
