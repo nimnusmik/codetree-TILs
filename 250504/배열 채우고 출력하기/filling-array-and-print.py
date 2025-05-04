e18f42c71ce8b6495b929da1d71b6e87e08b0110
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-04 14:08:25 +0900

    Title: 배열 채우고 출력하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250504/배열 채우고 출력하기/filling-array-and-print.py
+++ 250504/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,6 +1,4 @@
-chars = list(map(str,input().split()))
-new_list = [x for x in range(0,len(chars),-1)]
-print(chars.item())
+arr = list(input().split())
 
-
-     
+for i in rnage(9,-1,-1):
+    print(arr[i], end='')
