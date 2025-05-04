7260825a23b734a8124aecfa38152f36021bb183
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-04 15:12:27 +0900

    Title: 일의 자리 배열, Time: 39ms, Memory: 16MB, Status: Passed - Codetree

--- 250504/일의 자리 배열/array-with-units.py
+++ 250504/일의 자리 배열/array-with-units.py
@@ -1,7 +1,7 @@
 f, s = map(int, input().split())
-new_list = [f, s, f + s]  # 세 번째 항은 mod 없이 그대로!
+new_list = [f, s]  # 세 번째 항은 mod 없이 그대로!
 
-for i in range(3, 10):
+for i in range(2, 10):
     new_num = (new_list[i - 1] + new_list[i - 2]) % 10
     new_list.append(new_num)
 
