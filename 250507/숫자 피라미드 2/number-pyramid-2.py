4569eb46ee05ec51fec03ac43570ef536a772277
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-07 00:12:23 +0900

    Title: 숫자 피라미드 2, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 250507/숫자 피라미드 2/number-pyramid-2.py
+++ 250507/숫자 피라미드 2/number-pyramid-2.py
@@ -1,8 +1,8 @@
 N = int(input())
 
-num = 1  # 출력할 숫자
-for i in range(1, N + 1):       # 줄 수만큼 반복 (1줄, 2줄, ..., N줄)
-    for j in range(i):          # 줄마다 숫자를 i개 출력
-        print(num, end=' ')     # 숫자 출력 후 줄바꿈 안 하고 한 칸 띄기
-        num += 1                # 숫자 1 증가
-     print()                     # 줄 바꾸기
+num = 1
+for i in range(1, N + 1):
+    for j in range(i):
+        print(num, end=' ')
+        num += 1
+    print()
