a4f6eee49be23be71f2f4487a48fabc72a124f2d
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-06 21:01:28 +0900

    Title: 약수가 세 개인 수, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250506/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250506/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -1,9 +1,13 @@
 start, end = map(int, input().split())
 
-# Please write your code here.
-for i in range(start, end+1):
-    Divisor = 0
-    for j in range(i):
-        if j % i == 0: 
-            Divisor += 1
-print(Divisor)
+count = 0  # 전체 소수 개수 세기용
+for i in range(start, end + 1):
+    Divisor = []
+    for j in range(1, i + 1):  # 여기! j는 1부터 i까지 가야 모든 약수 확인 가능해요
+        if i % j == 0:
+            Divisor.append(j)
+
+    if len(Divisor) == 3:  # 소수는 약수가 1과 자기자신 딱 2개
+        count += 1
+
+print(count)
