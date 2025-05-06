55fb59a7a60a461b5132c358de780b9ed8cf6675
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-06 17:24:00 +0900

    Title: 특정 수 맞히기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250506/특정 수 맞히기/catching-specific-number.py
+++ 250506/특정 수 맞히기/catching-specific-number.py
@@ -1,9 +1,8 @@
 while True:
     user_input = int(input())
     if user_input == 25: 
-        pritnt('Good')
+        print('Good')
         break
-    
     else:
         if user_input < 25: print('Higher')
         if user_input > 25: print('Lower')
