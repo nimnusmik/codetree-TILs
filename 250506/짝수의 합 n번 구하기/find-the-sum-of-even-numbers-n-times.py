c8a0fda7d2bee25a5d0c16c07738635adac80fd0
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-06 20:25:16 +0900

    Title: 짝수의 합 n번 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250506/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
+++ 250506/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -1,12 +1,8 @@
 N = int(input())
-sum = 0
 for _ in range(N):
     a, b = map(int, input().split())
-    for i in range(a,b):
+    sum = 0  # 이 줄을 for문 안으로 옮겨주세요!
+    for i in range(a, b + 1):
         if i % 2 == 0:
-            #print('i:',i)
             sum += i
-            #print(sum) 
     print(sum)
-
-#print(6+8+10+12+14+16+18+20+22+24+26+28)
