099e159b6723f3ac93e0fd4828bc92caac414e04
Seonmin Kim <91577581+nimnusmik@users.noreply.github.com>
2025-05-06 18:10:45 +0900

    Title: 정상적인 구구단, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250506/정상적인 구구단/normal-multiple.py
+++ 250506/정상적인 구구단/normal-multiple.py
@@ -2,5 +2,5 @@ n = int(input())
 
 for i in range(n):
     for j in range(n):
-        print(f'{i+1} * {j+1} = {(j+1)*(i+1)}' , end=" ")
+        print(f'{i+1} * {j+1} = {(j+1)*(i+1)}', end=", ")
     print()
